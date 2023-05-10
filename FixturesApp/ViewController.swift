bc434ca3491f1e8055846a3a66ce148a1ddae491
ucwealth <ucheokeke16@gmail.com>
2023-05-10 20:24:04 +0100

    create project

---
+++ FixturesApp/ViewController.swift
@@ -0,0 +1,14 @@
+//  Created on 10/05/2023
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
